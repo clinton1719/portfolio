76c4cd0f2eb7acefb71c131396daf14db74cf2c7
clinton1719 <clintoncleetus17@gmail.com>
2025-06-12 19:17:08 +0530

    Adding blog section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -47,7 +47,12 @@ export const HeroSection = () => {
                 <span className="h-[1px] w-8 sm:w-12 bg-blue-500"></span>
               </div>
               <p className="text-base sm:text-lg text-gray-600 max-w-2xl px-4 sm:px-0">
-              Results-driven Senior Software Engineer with 5+ years of experience building scalable, cloud-native systems using Java, Spring Boot, and AWS Serverless. I’ve led cost-optimization efforts cutting cloud spend by up to 30%, contribute to open source, and share engineering insights through technical blogs on system design, Java, and AWS.
+                Results-driven Senior Software Engineer with 5+ years of
+                experience building scalable, cloud-native systems using Java,
+                Spring Boot, and AWS Serverless. I’ve led cost-optimization
+                efforts cutting cloud spend by up to 30%, contribute to open
+                source, and share engineering insights through technical blogs
+                on system design, Java, and AWS.
               </p>
             </div>
 
@@ -90,9 +95,43 @@ export const HeroSection = () => {
             </a>
             <a
               href="#contact"
-              className="px-6 sm:px-8 py-3 bg-gray-100 rounded-full hover:bg-gray-200 transition-colors border border-gray-200 hover:border-gray-300 text-center"
+              className="group relative inline-flex items-center justify-center"
             >
-              Get in Touch
+              <div className="absolute -inset-0.5 bg-gradient-to-r from-blue-500 to-purple-500 rounded-full blur opacity-50 group-hover:opacity-75 transition"></div>
+              <span className="relative px-6 sm:px-8 py-3 bg-white rounded-full inline-flex items-center justify-center w-full sm:w-auto">
+                Get in Touch
+                <svg
+                  className="w-4 h-4 ml-2 transform group-hover:translate-x-1 transition-transform"
+                  id="Layer_1"
+                  data-name="Layer 1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 122.88 84.04"
+                >
+                  <title>call-message</title>
+                  <path d="M34.11,3h83a5.8,5.8,0,0,1,5.79,5.79V70.27a5.76,5.76,0,0,1-1,3.25,2.32,2.32,0,0,1-.55.82,2.2,2.2,0,0,1-.54.38,5.78,5.78,0,0,1-3.7,1.35H68a15.44,15.44,0,0,0,.42-4.45h47.22L84.8,39.23,74.62,47.91h0a2.22,2.22,0,0,1-2.84,0L61.1,39.23h0l-9.69,9.71A12.4,12.4,0,0,0,47,47.07L57.64,36.41,37.91,20.32a14,14,0,0,0-.68-3.42l-.79-3.49L73.15,43.34,115.26,7.46H35.11L34.11,3ZM17.46,31a61.46,61.46,0,0,0,4.73,14.91A51.89,51.89,0,0,0,32.61,60.48a1.47,1.47,0,0,0,1.17.45,5.31,5.31,0,0,0,2-.67,17.91,17.91,0,0,0,2.1-1.36c3.14-2.18,7-4.89,10.29-1.85.08.07.12.14.2.2L58.84,68.78a1.13,1.13,0,0,1,.1.13,6.09,6.09,0,0,1,.79,5.77,14.31,14.31,0,0,1-3.94,5.76,13.76,13.76,0,0,1-7.94,3.46,29.8,29.8,0,0,1-8.28-.4,27.16,27.16,0,0,1-11.31-4.73,54.16,54.16,0,0,1-9.86-9.43l-.24-.29c-1.52-1.8-3.16-3.73-4.69-5.88A78.72,78.72,0,0,1,1,34.34C-.72,25.59-.37,16.85,3.33,9.62c2-4,5.06-7.2,9-8.69,3.44-1.32,7.51-1.34,12.13.63a1.67,1.67,0,0,1,1,1.24l3.73,16.58a4.57,4.57,0,0,1-.82,4.88,9.43,9.43,0,0,1-4.29,2.5c-.56.24-1.21.45-1.9.67-2.27.74-4.86,1.61-4.73,3.65v0Zm70.72,5.33,30.26,31.73V10.58L88.18,36.36Z" />
+                </svg>
+              </span>
+            </a>
+            <a
+              href="https://clinton1719.github.io/clinton-blogs/"
+              target="_blank"
+              className="group relative inline-flex items-center justify-center"
+            >
+              <div className="absolute -inset-0.5 bg-gradient-to-r from-blue-500 to-purple-500 rounded-full blur opacity-50 group-hover:opacity-75 transition"></div>
+              <span className="relative px-6 sm:px-8 py-3 bg-white rounded-full inline-flex items-center justify-center w-full sm:w-auto">
+                Read my blogs
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-4 h-4 ml-2 transform group-hover:translate-x-1 transition-transform"
+                  viewBox="0 0 122.88 90.02"
+                >
+                  <path
+                    d="M0 8.62c17.28-10.66 34.96-12.3 53.26 0v79.64c-12.65-9.37-31.03-8.24-53.26 0V8.62zm59.09.2h5.28c1.08 0 1.96.88 1.96 1.95v77.29c0 1.08-.88 1.96-1.96 1.96h-5.28a1.97 1.97 0 01-1.96-1.96V10.77c.01-1.07.89-1.95 1.96-1.95zm63.79-.2c-17.28-10.66-34.97-12.3-53.27 0v79.64c12.65-9.37 31.03-8.24 53.27 0V8.62z"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
             </a>
           </div>
         </div>
